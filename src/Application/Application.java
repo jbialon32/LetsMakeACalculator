fa406311f7b37f4f8069199e159a76247bd6780e
Sean <seanzee25@gmail.com>
2019-02-05 17:14:19 -0600

    Update Application.java
    Changed structure for outputting results.

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -14,8 +14,7 @@ public class Application {
         System.out.println("Loading data from file...");
         fileHandler.loadData(expressionList);
         System.out.println("Evaluating expressions...");
-        results = expressionList.generateResultList();
-        System.out.println("Saving results...");
-        fileHandler.saveResults(results);
+        // Output results to console.
+        expressionList.outputResults();
     }
 }
