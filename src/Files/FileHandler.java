7e69fa08d5eb10923cf967ab590e0bef782e1c33
Sean <seanzee25@gmail.com>
2019-02-05 17:15:52 -0600

    Update FileHandler.java
    Modified for getting the input file from project root. Can still set the input file name.

--- src/Files/FileHandler.java
+++ src/Files/FileHandler.java
@@ -1,11 +1,11 @@
 package Files;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
+import Expression.Expression;
 import Expression.ExpressionList;
 
 /**
@@ -14,24 +14,24 @@ import Expression.ExpressionList;
  *
  */
 public class FileHandler {
-    private String fileInputPath; // Name of input file
+    private String inputFileName; // Name of input file
     
     /**
      * Default constructor
      */
     public FileHandler() {
-        fileInputPath = "input.text";
+        inputFileName = "input.txt";
     }
     
     /**
      * 
      * @param inputFileName A String representing the name of the input file
      */
-    public FileHandler(String fileInputPath) {
-        this.fileInputPath = fileInputPath;
+    public FileHandler(String inputFileName) {
+        this.inputFileName = inputFileName;
     }
     
-    public void setFileInputPath(String value) { fileInputPath = value; }
+    public void setInputFileName(String value) { inputFileName = value; }
     
     /**
      * Loads data from file, filling an array list with Expression objects.
@@ -39,11 +39,18 @@ public class FileHandler {
      * @throws IOException
      */
     public void loadData(ExpressionList expressionList) throws IOException {
-        FileInputStream fileInput = new FileInputStream(fileInputPath);
+        FileInputStream fileInput = new FileInputStream(inputFileName);
         Scanner inputScanner = new Scanner(fileInput);
         
         
-        // TODO Load data from file and create Expression instances to add to list here.
+        // Load data from file and create Expression instances to add to list.
+        String expressionString;
+        Expression expression;
+        while(inputScanner.hasNext()) {
+            expressionString = inputScanner.nextLine();
+            expression = new Expression(expressionString);
+            expressionList.add(expression);
+        }
         
         // Close streams
         fileInput.close();
